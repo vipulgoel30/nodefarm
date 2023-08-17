e27e3efccfcb6b8a80d68a6b04272d79e8669bef
Vipul Goel <96120213+vipulgoel30@users.noreply.github.com>
2023-08-17 23:02:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ const server = http.createServer((req, res) => {
 const port = process.env.PORT || 7000
 
 // server listen
-server.listen(port, "127.0.0.1", () => {
+server.listen(port, () => {
     console.log(`Server started at ${port}`);
 });
